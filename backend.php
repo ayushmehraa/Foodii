df6559d9af1e1cc90c5619fc4e3281de24316ecd
ayushmehraa <85395442+ayushmehraa@users.noreply.github.com>
2021-11-08 15:06:43 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,12 @@
+<?php
+        if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+            // $email=$_POST['email'];
+            echo  '<div class="alert alert-success" role="alert">
+            <h4 class="alert-heading">Thank you for Contacting us</h4>
+            <p>Thank you for Contacting us</p>
+            <hr>
+            <p class="mb-0">we will try to reach you soon.</p>
+          </div>';
+            
+        }
+        ?>
